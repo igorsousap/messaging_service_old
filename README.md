9c1c4dc6e043747f7d63c78010eb7f193d64f29e
igorsousa14 <igor_sousa_pinto@hotmail.com>
2024-04-11 17:55:33 -0300

    add new project

---
+++ README.md
@@ -0,0 +1,4 @@
+# WebhookService
+
+**TODO: Add description**
+
---
+++ apps/persistence/lib/persistence/application.ex
@@ -0,0 +1,25 @@
+defmodule Persistence.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Persistence.Repo,
+      # Start the Telemetry supervisor
+      PersistenceWeb.Telemetry
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Persistence.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+end
